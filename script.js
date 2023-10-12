3a51edd9027f69f36288bdae0820da69b12cdfb0
Nate Watkins <natewatkinsca@gmail.com>
2023-10-11 17:31:45 -0700

    Working Flow Field w/ colors, lines, etc.

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ class Particle {
         this.maxLength = Math.floor(Math.random() * 200 + 10)
         this.angle = 0
         this.timer = this.maxLength * 2
+        //add more colors
+        this.colors = ["#e88c4a", "#f7a86f", "#eb6505", "#b04a02", "#ebb48d", "#1df500", "#f5aa0a"]
+        this.color = this.colors[Math.floor(Math.random() * this.colors.length)]
     }
     draw(context) {
         context.beginPath()
@@ -27,6 +30,7 @@ class Particle {
         for (let i = 0; i < this.history.length; i++) {
             context.lineTo(this.history[i].x, this.history[i].y)
         }
+        context.strokeStyle = this.color
         context.stroke()
     }
     update() {
@@ -61,18 +65,31 @@ class Particle {
 }
 
 class Effect {
-    constructor(width, height) {
-        this.width = width
-        this.height= height
+    constructor(canvas) {
+        this.canvas = canvas
+        this.width = this.canvas.width
+        this.height= this.canvas.height
         this.particles = []
-        this.numOfParticles = 300
-        this.cellSize = 20
+        //lines and boxes
+        this.numOfParticles = 1000
+        this.cellSize = 40
         this.rows
         this.cols
         this.flowField =[]
-        this.curve = 0.5
-        this.zoom = .11
+        //curve and zoom
+        this.curve = 3
+        this.zoom = 0.4
+        this.debug = false
         this.init()
+
+        window.addEventListener('keydown', e => {
+            if(e.key === 'd') this.debug = !this.debug
+        }) 
+
+        window.addEventListener("resize", e => {
+            console.log(e);
+            this.resize(e.target.innerWidth, e.target.innerHeight)
+        })
     }
     init() {
         //create flow field
@@ -81,18 +98,43 @@ class Effect {
         this.flowField = []
         for(let y = 0; y < this.rows; y++) {
             for(let x = 0; x < this.cols; x++) {
-                let angle = (Math.cos(x * this.zoom) + Math.sin(y * this.zoom)) * this.curve
+                //edit lines
+                let angle = (Math.cos(x * this.zoom) * Math.sin(y * this.zoom)) + this.curve
                 this.flowField.push(angle)
             }
         }
-        console.log(this.flowField);
 
         //creates particles 
         for( let i = 0; i < this.numOfParticles; i++) {
             this.particles.push(new Particle(this))
         }
     }
+    drawGrid(context) {
+        context.save()
+        context.strokeStyle = 'red'
+        context.lineWidth = 0.3
+        for(let c = 0; c < this.cols; c++) {
+            context.beginPath()
+            context.moveTo(this.cellSize * c, 0)
+            context.lineTo(this.cellSize * c, this.height)
+            context.stroke()
+        }
+        for(let r = 0; r < this.rows; r++){
+            context.beginPath()
+            context.moveTo(0, this.cellSize * r)
+            context.lineTo(this.width, this.cellSize * r)
+            context.stroke()
+        }
+        context.restore()
+    }
+    resize(width, height) {
+        this.canvas.width = width
+        this.canvas.height = height
+        this.width = this.canvas.width
+        this.height= this.canvas.height
+    }
     render(context) {
+        if (this.debug) this.drawGrid(context)
         this.particles.forEach(particle => {
             particle.draw(context)
             particle.update()
@@ -100,8 +142,8 @@ class Effect {
     }
 }
 
-const effect = new Effect(canvas.width, canvas.height)
-effect.render(ctx)
+const effect = new Effect(canvas)
+
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
